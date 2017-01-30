d6c3a348992c9177b422b8ba9ec3101ca5d809d8
Kristin <coldzonemt@yahoo.com>
2017-01-29 21:49:15 -0800

    remove vector class examples

--- vector_class.py
+++ vector_class.py
@@ -38,24 +38,3 @@ class Vector(object):
     def __eq__(self, v):
         return self.coordinates == v.coordinates
 
-my_v = Vector([1,2,3])
-my_v1 = Vector([2,3,4])
-my_v2 = Vector([2,3,4])
-
-v1 = Vector([-0.221, 7.437])
-v2 = Vector([8.813, -1.331, -6.247])
-v3 = Vector([5.581, -2.136])
-v4 = Vector([1.996, 3.108, -4.554])
-
-# print my_v1
-# print my_v1 == my_v2
-
-# print [my_v2.coordinates, my_v1.coordinates]
-# print my_v.add_vector(my_v2)
-# print my_v2.subtract_vectors(my_v)
-# print my_v.scalar_multiplication(10)
-
-print v1.magnitude()
-print v2.magnitude()
-print v3.normalize()
-print v4.normalize()
