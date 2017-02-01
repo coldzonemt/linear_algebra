6abaf2e57b7cebaeb45b73258c98bab50d8c3351
coldzonemt <coldzonemt@yahoo.com>
2017-02-01 13:29:02 -0800

    lunchtime wip dot product, angle between vectors

--- vector_class.py
+++ vector_class.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class Vector(object):
     def __init__(self, coordinates):
         try:
@@ -28,8 +30,23 @@ class Vector(object):
         return sum([x**2 for x in self.coordinates])**0.5
 
     def normalize(self): 
-        # consider if self is zero, so wrap in try / except with ZeroDivisionError
-        return self.scalar_multiplication(1/self.magnitude())
+        try: 
+            return self.scalar_multiplication(1/self.magnitude())
+        except ZeroDivisionError: 
+            raise Exception('Cannot normalize the zero vector')
+
+    def dot_product(self, v1):
+        return sum([x*y for x,y in zip(self.coordinates, v1.coordinates)])
+
+    def angle_btwn_vectors(self, v1): 
+        numerator = self.dot_product(v1)
+        a = Vector(self.normalize())
+        b = Vector(v1.normalize())
+        denominator = a.dot_product(b)
+        theta_term = numerator/denominator
+        print "theta_term: ", theta_term
+        theta = np.arccos(theta_term)
+        return theta
 
     def __str__(self):
         return 'Vector: {}'.format(self.coordinates)
--- vector_test_cases.py
+++ vector_test_cases.py
@@ -4,14 +4,32 @@ Test info for the Vector Class
 
 from vector_class import Vector 
 
-my_v = Vector([1,2,3])
-my_v1 = Vector([2,3,4])
-my_v2 = Vector([2,3,4])
+v1 = Vector([7.887, 4.138])
+w1 = Vector([-8.802, 6.776])
 
-v1 = Vector([-0.221, 7.437])
-v2 = Vector([8.813, -1.331, -6.247])
-v3 = Vector([5.581, -2.136])
-v4 = Vector([1.996, 3.108, -4.554])
+v2 = Vector([-5.955, -4.904, -1.874])
+w2 = Vector([-4.496, -8.755, 7.103])
+
+v3 = Vector([3.183, -7.627])
+w3 = Vector([-2.668, 5.319])
+
+v4= Vector([7.35, 0.221, 5.188])
+w4 = Vector([2.751, 8.259, 3.985])
+
+# print v1.dot_product(w1)
+# print v2.dot_product(w2)
+
+print v3.angle_btwn_vectors(w3)
+print v4.angle_btwn_vectors(w4)
+
+# my_v = Vector([1,2,3])
+# my_v1 = Vector([2,3,4])
+# my_v2 = Vector([2,3,4])
+
+# v1 = Vector([-0.221, 7.437])
+# v2 = Vector([8.813, -1.331, -6.247])
+# v3 = Vector([5.581, -2.136])
+# v4 = Vector([1.996, 3.108, -4.554])
 
 # print my_v1
 # print my_v1 == my_v2
@@ -21,7 +39,7 @@ v4 = Vector([1.996, 3.108, -4.554])
 # print my_v2.subtract_vectors(my_v)
 # print my_v.scalar_multiplication(10)
 
-print v1.magnitude()
-print v2.magnitude()
-print v3.normalize()
-print v4.normalize()
+# print v1.magnitude()
+# print v2.magnitude()
+# print v3.normalize()
+# print v4.normalize()
